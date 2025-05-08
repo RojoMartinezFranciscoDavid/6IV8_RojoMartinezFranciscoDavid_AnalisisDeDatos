6e50f312d388ef30573a14824922dc237769ae01
RojoMartiezFranciscoDavid <frojom2200@alumno.ipn.mx>
2025-05-08 04:57:39 -0600

    ajustes de chava

--- MachineLearning/DistanciaEntrePuntos.py
+++ MachineLearning/DistanciaEntrePuntos.py
@@ -2,47 +2,70 @@ import numpy as np
 import pandas as pd
 from scipy.spatial import distance
 
-
-
-tiendas={
-    'Tienda A':[2, 3],
-    'Tienda B':[5, 4],
-    'Tienda C':[1, 1],
-    'Tienda D':[6, 7],
-    'Tienda E':[3, 5],
-    'Punto F': (8, 2),
-    'Punto G': (4, 6),
-    'Punto H': (2, 1)
-    
+puntos = {
+    'Punto A': [2, 3],
+    'Punto B': [5, 4],
+    'Punto C': [1, 1],
+    'Punto D': [6, 7],
+    'Punto E': [3, 5],
+    'Punto F': [8, 2],
+    'Punto G': [4, 6],
+    'Punto H': [2, 1]
 }
 
+df_puntos = pd.DataFrame(puntos).T
+df_puntos.columns = ['X', 'Y']
+print("Coordenadas de los puntos:")
+print(df_puntos)
 
-df_tiendas= pd.DataFrame(tiendas).T
-df_tiendas.columns=['X','Y']
-print("coordenadas de las tiendas")
-print(df_tiendas)
+distancias_eu = pd.DataFrame(index=df_puntos.index, columns=df_puntos.index)
+distancias_mh = pd.DataFrame(index=df_puntos.index, columns=df_puntos.index)
+distancias_ch = pd.DataFrame(index=df_puntos.index, columns=df_puntos.index)
 
+for i in df_puntos.index:
+    for j in df_puntos.index:
+        # Distancia Euclidiana
+        distancias_eu.loc[i, j] = distance.euclidean(df_puntos.loc[i], df_puntos.loc[j])
+        # Distancia Manhattan
+        distancias_mh.loc[i, j] = distance.cityblock(df_puntos.loc[i], df_puntos.loc[j])
+        # Distancia Chebyshev
+        distancias_ch.loc[i, j] = distance.chebyshev(df_puntos.loc[i], df_puntos.loc[j])
 
-
-distancias_eu=pd.DataFrame(index=df_tiendas.index, columns=df_tiendas.index)
-distancias_mh=pd.DataFrame(index=df_tiendas.index, columns=df_tiendas.index)
-distancias_ch=pd.DataFrame(index=df_tiendas.index, columns=df_tiendas.index)
-
-
-
-for i in df_tiendas.index:
-    for j in df_tiendas.index:
-        #Distancia Euclidiana:
-        distancias_eu.loc[i,j]=distance.euclidean(df_tiendas.loc[i],df_tiendas.loc[j])
-        #Distancia Manhattan
-        distancias_mh.loc[i,j]=distance.cityblock(df_tiendas.loc[i],df_tiendas.loc[j])
-        #Distancia Cheby
-        distancias_ch.loc[i,j]=distance.chebyshev(df_tiendas.loc[i],df_tiendas.loc[j])
-        
-
-print("\nDistancias Euclideanas entre las tiendas: ")
+print("\nDistancias Euclidianas entre los puntos:")
 print(distancias_eu)
-print("\nDistancias Manhattan entre las tiendas: ")
+print("\nDistancias Manhattan entre los puntos:")
 print(distancias_mh)
-print("\nDistancias Chebyshev entre las tiendas: ")
+print("\nDistancias Chebyshev entre los puntos:")
 print(distancias_ch)
+
+def encontrar_extremos(matriz_distancia):
+    matriz_temp = matriz_distancia.copy()
+    
+    np.fill_diagonal(matriz_temp.values, np.nan)
+    
+    min_dist = np.nanmin(matriz_temp.values)
+    min_idx = np.where(matriz_temp.values == min_dist)
+    punto1_min, punto2_min = matriz_temp.index[min_idx[0][0]], matriz_temp.columns[min_idx[1][0]]
+    
+    max_dist = np.nanmax(matriz_temp.values)
+    max_idx = np.where(matriz_temp.values == max_dist)
+    punto1_max, punto2_max = matriz_temp.index[max_idx[0][0]], matriz_temp.columns[max_idx[1][0]]
+    
+    return (punto1_min, punto2_min, min_dist), (punto1_max, punto2_max, max_dist)
+
+print("\n=== RESULTADOS DEL ANÁLISIS ===")
+
+min_par_eu, max_par_eu = encontrar_extremos(distancias_eu)
+print("\nDistancia Euclidiana:")
+print(f"Puntos más cercanos: {min_par_eu[0]} y {min_par_eu[1]} con distancia {min_par_eu[2]:.4f}")
+print(f"Puntos más alejados: {max_par_eu[0]} y {max_par_eu[1]} con distancia {max_par_eu[2]:.4f}")
+
+min_par_mh, max_par_mh = encontrar_extremos(distancias_mh)
+print("\nDistancia Manhattan:")
+print(f"Puntos más cercanos: {min_par_mh[0]} y {min_par_mh[1]} con distancia {min_par_mh[2]:.4f}")
+print(f"Puntos más alejados: {max_par_mh[0]} y {max_par_mh[1]} con distancia {max_par_mh[2]:.4f}")
+
+min_par_ch, max_par_ch = encontrar_extremos(distancias_ch)
+print("\nDistancia Chebyshev:")
+print(f"Puntos más cercanos: {min_par_ch[0]} y {min_par_ch[1]} con distancia {min_par_ch[2]:.4f}")
+print(f"Puntos más alejados: {max_par_ch[0]} y {max_par_ch[1]} con distancia {max_par_ch[2]:.4f}")
